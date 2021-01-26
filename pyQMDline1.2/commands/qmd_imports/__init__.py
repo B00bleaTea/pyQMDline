ef3c84647bfaacea51b8bf88765a91cb0e943e3b
B00bleaTea <71613062+B00bleaTea@users.noreply.github.com>
2021-01-26 16:52:30 +0200

    pyQMDline1.2

---
+++ pyQMDline1.2/commands/qmd_imports/__init__.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import json
+import shutil
+import requests
+import subprocess
+import platform
+import webbrowser
+import pytube
+import psutil
+import pprint
+import time
+
+from tabulate import tabulate
+from imports import Out, coloured
+from colorama import Fore, Style
+from ast import literal_eval
+from ast import literal_eval
---
+++ pyQMDline1.2/system/system-folders/EMPTY_TRASH.py
@@ -0,0 +1,2 @@
+os.mkdir('./system/system-folders/trash') if not os.path.exists('./system/system-folders/trash') else shutil.rmtree(
+	'./system/system-folders/trash') or os.mkdir('./system/system-folders/trash')
